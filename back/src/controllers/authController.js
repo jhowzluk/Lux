10e9ece04b8b63b82ac5ca9f5cdd609c63791ba4
jhowzluk <jhonatanluksik@gmail.com>
2025-11-25 18:54:06 -0300

    feat: alterando protocolo do nodemailer

--- back/src/controllers/authController.js
+++ back/src/controllers/authController.js
@@ -5,9 +5,11 @@ const nodemailer = require('nodemailer');
 const saltRounds = 10;
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail', 
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
     auth: {
-        user: process.env.EMAIL_USER, 
+        user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
     }
 });
